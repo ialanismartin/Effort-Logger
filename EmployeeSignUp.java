f2f632bc3f48b95dba064a9f1bfa146cbe94105d
Junayd Lateef <66799683+JunaydL@users.noreply.github.com>
2023-04-25 15:07:57 -0700

    Updated log in and sign up with credential check
    The files uploaded have the credential checks needed. I changed the default username  to "effortlogger" and password to "1234effort" so that way it fit the credential requirements.

--- EmployeeSignUp.java
+++ EmployeeSignUp.java
@@ -39,10 +39,32 @@ public class EmployeeSignUp {
 	public void userSignUp(ActionEvent event) throws IOException
 	{
 		checkSignUp();
+		/*
+		 * Pseudocode for sign up
+		 * 
+		 * if(checkSignUp){ //if the credentials are good
+		 * 	//add the user in here
+		 * }
+		 * 
+		 */
 	}
-	//this is where Junayd's prototype can be inputed 
-	private void checkSignUp() throws IOException
+	
+	//this is where Junayd's prototype can be inputed
+	//This method uses all of the helper methods to determine if the inputs are valid
+	private boolean checkSignUp() throws IOException
 	{
+		String user = usernameTextField.getText();
+		String pass = usernameTextField.getText();
+		
+		if(!lengthCheck(user) || !lengthCheck(pass)) {//checks for length of the input
+			return false;
+		}else if(numCount(user) < 1 || numCount(pass) < 2) {//checks numbers of the input
+			return false;
+		}else if(!invalidCheck(user) || !invalidCheck(pass)) {//checks if there are any invalid characters
+			return false;
+		}else {
+			return true;
+		}
 		
 	}
 	//action handler for settings button 
@@ -50,4 +72,37 @@ public class EmployeeSignUp {
 	{
 		m.changeScene("ManagerDashboard.fxml");
 	}
+	
+	//The following methods are used to check the code for any invalid inputs
+		//checks for invalid characters
+		public boolean invalidCheck(String s) {
+			if(s.contains("*") || s.contains("!") || s.contains("&") || s.contains("=") || s.contains("#") || s.contains("%")) {
+				//if invalid characters exist the return false 
+				return false;
+			}//if no invalid characters exist then return true
+			return true;
+		}
+
+		//checks the length of the string so that way nothing suspicious is inputted
+		public boolean lengthCheck(String s) {
+			if(s.length() <= 6 || s.length() > 20) {
+				//if the string length does not fit the range then return false
+					return false;
+				}
+			//if character range fits then return true
+				return true;
+			}
+		
+		//used to check the number of integers in the string
+		public static int numCount(String s) {
+			int numInts = 0; //records number of ints in the string
+			
+			//for loop is used to search through the entire string
+			for(int i = 0; i < s.length(); i++) {
+				if(Character.isDigit(s.charAt(i))) { //checks if current char is a number
+					numInts++; //increments digit count
+				}
+			}
+			return numInts; //return number of integers
+		  }
 }
--- LogIn.java
+++ LogIn.java
@@ -39,20 +39,45 @@ public class LogIn {
 	private void checkLogin() throws IOException
 	{
 		Main m = new Main();
-		if(username.getText().toString().equals("effort") && password.getText().toString().equals("123"))
-		{
-			wrongLogIn.setText("Success!");
-			m.changeScene("Dashboard.fxml");
-		}
-		else if(username.getText().toString().isEmpty() && password.getText().toString().isEmpty())
+		if(username.getText().toString().isEmpty() && password.getText().toString().isEmpty())//check if empty
 		{
 			wrongLogIn.setText("Please enter your credentials.");
+		}
+		else if(!lengthCheck(username.getText()) || !lengthCheck(password.getText())) //check length 
+		{
+			wrongLogIn.setText("Credentials must be 7-20 chars.");
+		}else if(!invalidCheck(username.getText()) || !invalidCheck(password.getText())) //check if invalid
+		{
+			wrongLogIn.setText("Invalid chars detected.");
+		}else if(username.getText().toString().equals("effortlogger") && password.getText().toString().equals("1234effort")) { //compare credentials
+			wrongLogIn.setText("Success!");
+			m.changeScene("Dashboard.fxml");
 		}
 		else
 		{
 			wrongLogIn.setText("Incorrect username or password!");
 		}
-		
 	}
+	
+	//The following methods are used to check the code for any invalid inputs
+	//checks for invalid characters
+	public boolean invalidCheck(String s) {
+		if(s.contains("*") || s.contains("!") || s.contains("&") || s.contains("=") || s.contains("#") || s.contains("%")) {
+			//if invalid characters exist the return false 
+			return false;
+		}//if no invalid characters exist then return true
+		return true;
+	}
+
+	//checks the length of the string so that way nothing suspicious is inputted
+	public boolean lengthCheck(String s) {
+		if(s.length() <= 6 || s.length() > 20) {
+			//if the string length does not fit the range then return false
+				return false;
+			}
+		//if character range fits then return true
+			return true;
+		}
+
 }
 
