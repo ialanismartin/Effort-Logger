bf2c29a1d95c6d621adeab8d2fbcfa6eb00ad2fe
Balassa1 <99202295+Balassa1@users.noreply.github.com>
2023-04-30 16:41:51 -0700

    Add files via upload

--- EmployeeSignUp.java
+++ EmployeeSignUp.java
@@ -5,9 +5,11 @@ import java.io.IOException;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 
 public class EmployeeSignUp {
 	
@@ -30,6 +32,8 @@ public class EmployeeSignUp {
 	private TextField passwordTextField;
 	@FXML
 	private TextField jobTitleTextField;
+	@FXML
+	private Label errorMessageLabel;
 	
 	public EmployeeSignUp()
 	{
@@ -41,8 +45,50 @@ public class EmployeeSignUp {
 		checkSignUp();
 	}
 	//this is where Junayd's prototype can be inputed 
-	private void checkSignUp() throws IOException
+	private boolean checkSignUp() throws IOException
 	{
+		
+		errorMessageLabel.setMaxWidth(Double.MAX_VALUE);
+		AnchorPane.setLeftAnchor(errorMessageLabel, 0.0);
+		AnchorPane.setRightAnchor(errorMessageLabel, 0.0);
+		errorMessageLabel.setAlignment(Pos.CENTER);
+		
+		
+		String user = usernameTextField.getText();
+		String pass = passwordTextField.getText();
+		
+		
+		if(usernameTextField.getText().toString().isEmpty() || passwordTextField.getText().toString().isEmpty() || jobTitleTextField.getText().toString().isEmpty())
+		{
+			errorMessageLabel.setText("Please provide an input for all required fields.");
+			return false;
+		}
+		
+		if(!lengthCheck(user) || !lengthCheck(pass))
+		{
+			errorMessageLabel.setText("Incorrect length for credentials. Username and password must be 7-20 characters long!");
+			return false;
+		}
+		
+		else if(numCount(user) < 1 || numCount(pass) < 2)
+		{
+			errorMessageLabel.setText("Username must contain at least 2 digits and password must contain at least 1 digit!");
+			return false;
+		}
+		else if(!invalidCheck(user) || !invalidCheck(pass))
+		{
+			errorMessageLabel.setText("Employee credentials cannot contain the following characters: '*', '&', '=', '%', '#', '!' ");
+			return false;
+		}
+		else if(Database.db.findUser(user,pass))
+		{
+			errorMessageLabel.setText("Employee already exists. Please try again.");
+		}
+		else 
+		{
+			errorMessageLabel.setText("Success! New employee created.");
+		}
+		return true;
 		
 	}
 	//action handler for settings button 
